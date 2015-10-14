0c542f167c3c52ff9849dbf9c4788a197011bd4a
strap8 <nateinthegame@gmail.com>
2015-10-13 21:38:45 -0700

    Formated SpellBook Class

--- SpellBook.hpp
+++ SpellBook.hpp
@@ -1,1 +1,22 @@
+#ifndef SPELLBOOK_HPP
+#define SPELLBOOK_HPP
+
+#include <string>
+
+class SpellBook 
+{
+public:
+
+	// Usual constructor
+	SpellBook() 
+	{
+		
+	}
+
+	SpellBook()
+	{
+	}
+};
+
+#endif /*  SPELLBOOK_HPP */
 
