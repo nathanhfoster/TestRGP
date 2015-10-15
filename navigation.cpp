7a7838b940e47edabd76ff55fb756992bba9e423
Fei <thomasshepherd2002@gmail.com>
2015-10-14 22:21:43 -0700

    Create navigation.cpp

---
+++ navigation.cpp
@@ -0,0 +1,2 @@
+create a singleton class which maintains a 2 dimmentional integer array. Have functions that creates entities
+inside the navigation map, move entities around, and prints out the array in a stylized manner. 
